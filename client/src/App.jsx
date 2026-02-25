566f6ddb974c4ba5cad22e02ebc4711ccc2552e2
Deyner97 <deynerveroes9712@gmail.com>
2026-02-24 20:29:19 -0400

    añadiendo todo de manera local

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Proyecto 3: Mi Aplicación Full Stack</h1>
+      <p>¡Esta página ya está corriendo en React!</p>
+      
+      <div style={{ border: '1px solid #ccc', padding: '10px', marginTop: '20px' }}>
+        <h3>Estado del Backend:</h3>
+        <p>Próximamente conectaremos Node.js y Python aquí...</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
